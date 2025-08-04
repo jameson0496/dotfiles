60876f051d19a0be2abb4a60ecf67293dbfee80c
jameson0496 <jamie.d.wilson1964@gmail.com>
2025-08-03 22:16:44 -0400

    added picom kill/restart script into powermenu.sh, fixing the months-old black screen problem. yay.

--- scripts/.scripts/powermenu.sh
+++ scripts/.scripts/powermenu.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-# Power menu script using tofi
-
 CHOSEN=$(printf "Suspend\nReboot\nShutdown\nLock" | rofi -dmenu -i)
-# CHOSEN=$(printf "Suspend\nReboot\nShutdown\nLock" | rofi -dmenu -i -theme-str '@import "glue_pro_blue.rasi"')
 
 case "$CHOSEN" in
-	"Suspend") systemctl suspend ;;
+	"Suspend") pkill picom & systemctl suspend && sleep 2 && picom & ;;
 	"Lock") i3lock-fancy ;;
 	"Reboot") reboot ;;
 	"Shutdown") poweroff ;;
